dcd3e927babcb8f52a0e164d1c489b536075d12e
Lindsey Peterson <55323822+Pederlaa@users.noreply.github.com>
2019-09-29 21:47:04 -0500

    Initial commit

---
+++ HardwareMapping.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareMapping
+{
+    /* Public OpMode members. */
+    public DcMotor  leftmotor   = null;
+    public DcMotor  rightmotor  = null;
+    public Servo    platformServo    = null;
+
+    public Gyroscope imu = null;
+    //public DigitalChannel digitalTouch = null;
+    //public DistanceSensor sensorColorRange = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareMapping(){
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftmotor  = hwMap.get(DcMotor.class, "leftmotor");
+        rightmotor = hwMap.get(DcMotor.class, "rightmotor");
+        leftmotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightmotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        leftmotor.setPower(0);
+        rightmotor.setPower(0);
+
+        // Define and initialize ALL installed servos.
+        platformServo  = hwMap.get(Servo.class, "platformServo");
+
+        //Define rest.
+        imu = hwMap.get(Gyroscope.class, "imu");
+        //digitalTouch = hwMap.get(DigitalChannel.class, "digitalTouch");
+        //sensorColorRange = hwMap.get(DistanceSensor.class, "sensorColorRange");
+    }
+}
---
+++ TeleOpPractice.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+
+public class TeleOpPractice extends LinearOpMode {
+
+    HardwareMapping robot = new HardwareMapping();
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        // run until the end of the match (driver presses STOP)
+        double tgtPowerleft = 0;
+        double tgtPowerright = 0;
+        while (opModeIsActive()) {
+            tgtPowerleft = -this.gamepad1.left_stick_y;
+            robot.leftmotor.setPower(tgtPowerleft);
+            tgtPowerright = -this.gamepad1.right_stick_y;
+            robot.rightmotor.setPower(tgtPowerright);
+            // check to see if we need to move the servo.
+            if (gamepad2.y) {
+                // move to 0 degrees.
+                robot.platformServo.setPosition(0);
+            } else if (gamepad2.x) {
+                // move to 90 degrees.
+                robot.platformServo.setPosition(0.5);
+            } else if (gamepad2.a) {
+                // move to 180 degrees.
+                robot.platformServo.setPosition(1);
+                telemetry.addData("Left Power", tgtPowerleft);
+                telemetry.addData("Right Power", tgtPowerright);
+                telemetry.addData( caption: "Platform Servo Position", robot.platformServo.getPosition())
+                telemetry.addData("Status", "Running");
+                telemetry.update();
+            }
+        }
+    }
+}
